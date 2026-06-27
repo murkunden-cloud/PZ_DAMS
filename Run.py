b2de9b09c71677c74f466d31ddf3b7ffe9cc8ded
Nagesh M <asstgmanagerned@gmail.com>
2026-06-28 03:30:51 +0530

    Update Render configuration

--- Run.py
+++ Run.py
@@ -2,6 +2,8 @@ from DC_DataLoader import DCDataLoader
 from DC_WebApp import run_server
 
 if __name__ == '__main__':
+    import os
     loader = DCDataLoader()
     # The loader is now aware of config.ini and PyInstaller APP_DIR
-    run_server(loader, host="127.0.0.1", port=5000, open_browser=True)
+    port = int(os.environ.get("PORT", 5000))
+    run_server(loader, host="0.0.0.0", port=port, open_browser=False)
